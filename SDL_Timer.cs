9031c4fddd37fd39d908e7780cc46683377afa2b
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2024-10-27 17:33:33 +0100

    Updated Timer Comments

--- SDL_Timer.cs
+++ SDL_Timer.cs
@@ -1,27 +1,43 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 
 namespace SDL3
 {
     public static partial class SDL
     {
+
+        /// <summary>Get the number of milliseconds since SDL library initialization.</summary>
+        /// <returns>The number of milliseconds since the SDL library initialized.</returns>
         [DllImport(nativeLibraryName, EntryPoint = "SDL_GetTicks", CallingConvention = CallingConvention.Cdecl)]
         public static extern ulong GetTicks();
 
+        /// <summary>Get the number of nanoseconds since SDL library initialization.</summary>
+        /// <returns>The number of nanaseconds since the SDL library initialized.</returns>
         [DllImport(nativeLibraryName, EntryPoint = "SDL_GetTicksNS", CallingConvention = CallingConvention.Cdecl)]
         public static extern ulong GetTickNS();
 
+        /// <summary>Get the current value of the high resolution counter.</summary>
+        /// <returns>The current counter value.</returns>
         [DllImport(nativeLibraryName, EntryPoint = "SDL_GetPerformanceCounter", CallingConvention = CallingConvention.Cdecl)]
         public static extern ulong GetPerformanceCounter();
 
+        /// <summary>Get the count per second of the high resolution counter.</summary>
+        /// <returns>A platform-specific count per second.</returns>
         [DllImport(nativeLibraryName, EntryPoint = "SDL_GetPerformanceFrequency", CallingConvention = CallingConvention.Cdecl)]
         public static extern ulong GetPerformanceFrequency();
 
+        /// <summary>Wait a specified number of milliseconds before returning.</summary>
+        /// <param name="milliseconds">The number of milliseconds to delay.</param>
         [DllImport(nativeLibraryName, EntryPoint = "SDL_Delay", CallingConvention = CallingConvention.Cdecl)]
         public static extern void Delay(uint milliseconds);
 
+        /// <summary>Wait a specified number of nanoseconds before returning.</summary>
+        /// <param name="nanoseconds">The number of nanoseconds to delay.</param>
         [DllImport(nativeLibraryName, EntryPoint = "SDL_DelayNS", CallingConvention = CallingConvention.Cdecl)]
         public static extern void DelayNS(ulong nanoseconds);
 
+        /// <summary>Wait a sepcified number of nanoseconds before returning.</summary>
+        /// <param name="nanoseconds">The number of nanseconds to delay.</param>
         [DllImport(nativeLibraryName, EntryPoint = "SDL_DelayPrecise", CallingConvention = CallingConvention.Cdecl)]
         public static extern void DelayPrecise(ulong nanoseconds);
 
