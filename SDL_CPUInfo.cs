c0db1bc677cad5defb6eba68a6b80808c1286c17
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2024-10-27 17:02:31 +0100

    CPUInfo complete

--- SDL_CPUInfo.cs
+++ SDL_CPUInfo.cs
@@ -15,42 +15,73 @@ namespace SDL3
         public static extern int GetCPUCacheLineSize();
 
 
+        /// <summary>Determine whether thr CPU has AltiVec features.</summary>
+        /// <returns>True of the CPU has AlitVec features, false otherwise.</returns>
         [DllImport(nativeLibraryName, EntryPoint = "SDL_HasAltiVec", CallingConvention = CallingConvention.Cdecl)]
         public static extern bool HasAltiVec();
 
+        /// <summary>Determine whether thr CPU has MMX features.</summary>
+        /// <returns>True of the CPU has MMX features, false otherwise.</returns>
         [DllImport(nativeLibraryName, EntryPoint = "SDL_HasMMX", CallingConvention = CallingConvention.Cdecl)]
         public static extern bool HasMMX();
 
+        /// <summary>Determine whether thr CPU has SSE features.</summary>
+        /// <returns>True of the CPU has SSE features, false otherwise.</returns>
         [DllImport(nativeLibraryName, EntryPoint = "SDL_HasSSE", CallingConvention = CallingConvention.Cdecl)]
         public static extern bool HasSSE();
 
+        /// <summary>Determine whether thr CPU has SSE2 features.</summary>
+        /// <returns>True of the CPU has SSE2 features, false otherwise.</returns>
         [DllImport(nativeLibraryName, EntryPoint = "SDL_HasSSE2", CallingConvention = CallingConvention.Cdecl)]
         public static extern bool HasSSE2();
 
+        /// <summary>Determine whether thr CPU has SSE3 features.</summary>
+        /// <returns>True of the CPU has SSE3 features, false otherwise.</returns>
         [DllImport(nativeLibraryName, EntryPoint = "SDL_HasSSE3", CallingConvention = CallingConvention.Cdecl)]
         public static extern bool HasSSE3();
 
+        /// <summary>Determine whether thr CPU has SSE41 features.</summary>
+        /// <returns>True of the CPU has SSE41 features, false otherwise.</returns>
         [DllImport(nativeLibraryName, EntryPoint = "SDL_HasSSE41", CallingConvention = CallingConvention.Cdecl)]
         public static extern bool HasSSE41();
 
+        /// <summary>Determine whether thr CPU has SSE42 features.</summary>
+        /// <returns>True of the CPU has SSE42 features, false otherwise.</returns>
         [DllImport(nativeLibraryName, EntryPoint = "SDL_HasSSE42", CallingConvention = CallingConvention.Cdecl)]
         public static extern bool HasSSE42();
 
+        /// <summary>Determine whether thr CPU has AVX features.</summary>
+        /// <returns>True of the CPU has AVX features, false otherwise.</returns>
         [DllImport(nativeLibraryName, EntryPoint = "SDL_HasAVX", CallingConvention = CallingConvention.Cdecl)]
         public static extern bool HasAVX();
 
+        /// <summary>Determine whether thr CPU has AVX2 features.</summary>
+        /// <returns>True of the CPU has AVX2 features, false otherwise.</returns>
         [DllImport(nativeLibraryName, EntryPoint = "SDL_HasAVX2", CallingConvention = CallingConvention.Cdecl)]
         public static extern bool HasAVX2();
 
+        /// <summary>Determine whether thr CPU has AVX-512F features.</summary>
+        /// <returns>True of the CPU has AVX-512F features, false otherwise.</returns>
+        [DllImport(nativeLibraryName, EntryPoint = "SDL_HasAVX512F", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool HasAVX512F();
+
+        /// <summary>Determine whether thr CPU has ARM SIMD (ARMv6) features.</summary>
+        /// <returns>True of the CPU has ARM SIMD features, false otherwise.</returns>
         [DllImport(nativeLibraryName, EntryPoint = "SDL_HasARMSIMD", CallingConvention = CallingConvention.Cdecl)]
         public static extern bool HasARMSIMD();
 
+        /// <summary>Determine whether thr CPU has NEON (ARM SIMD) features.</summary>
+        /// <returns>True of the CPU has ARM NEON features, false otherwise.</returns>
         [DllImport(nativeLibraryName, EntryPoint = "SDL_HasNEON", CallingConvention = CallingConvention.Cdecl)]
         public static extern bool HasNEON();
 
+        /// <summary>Determine whether thr CPU has LSX (LOONGARCH SIMD) features.</summary>
+        /// <returns>True of the CPU has LOONGARCH LSX features, false otherwise.</returns>
         [DllImport(nativeLibraryName, EntryPoint = "SDL_HasLSX", CallingConvention = CallingConvention.Cdecl)]
         public static extern bool HasLSX();
 
+        /// <summary>Determine whether thr CPU has LASX (LOONGARCH SIMD) features.</summary>
+        /// <returns>True of the CPU has LOONGARCH LASX features, false otherwise.</returns>
         [DllImport(nativeLibraryName, EntryPoint = "SDL_HasLASX", CallingConvention = CallingConvention.Cdecl)]
         public static extern bool HasLASX();
 
