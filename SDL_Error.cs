db96720c3c89ee51f5deee8a9234f56bedad9b13
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2024-10-27 17:26:31 +0100

    Update SDL_Error.cs

--- SDL_Error.cs
+++ SDL_Error.cs
@@ -7,22 +7,24 @@ namespace SDL3
     {
        
         [DllImport(nativeLibraryName, EntryPoint = "SDL_SetError", CallingConvention = CallingConvention.Cdecl)]
-        static extern unsafe void Internal_SetError(byte* fmtAndArglist);
+        static extern unsafe bool Internal_SetError(byte* fmtAndArglist);
 
         /// <summary>Set the SDL error message for the current thread.</summary>
-        /// <param name="fmtAndArglist">Additional parameters matching % tokens in the fmt string, if any.</param>
-        public static unsafe void SetError(string fmtAndArglist)
+        /// <param name="fmtAndArglist">The error message</param>
+        /// <returns>False</returns>
+        public static unsafe bool SetError(string fmtAndArglist)
         {
             int utf8FmtAndArlistBufferSize = Utility.UTF8Size(fmtAndArglist);
             byte* utf8FmtAndArgList = stackalloc byte[utf8FmtAndArlistBufferSize];
-            Internal_SetError(Utility.UTF8Encode(fmtAndArglist, utf8FmtAndArgList, utf8FmtAndArlistBufferSize));
+            return Internal_SetError(Utility.UTF8Encode(fmtAndArglist, utf8FmtAndArgList, utf8FmtAndArlistBufferSize));
         }
 
         // TODO SDL_SetErrorV
 
         /// <summary>Set an error indicating that memory allocation failed.</summary>
+        /// <returns>False</returns>
         [DllImport(nativeLibraryName, EntryPoint = "SDL_OutOfMemory", CallingConvention = CallingConvention.Cdecl)]
-        public static extern void OutOfMemory();
+        public static extern bool OutOfMemory();
 
         [DllImport(nativeLibraryName, EntryPoint = "SDL_GetError", CallingConvention = CallingConvention.Cdecl)]
         static extern IntPtr Internal_GetError();
@@ -32,9 +34,8 @@ namespace SDL3
         public static string GetError() => Marshal.PtrToStringUTF8(Internal_GetError());
 
         /// <summary>Clear any previous error message for this thread.</summary>
+        /// <returns>True</returns>
         [DllImport(nativeLibraryName, EntryPoint = "SDL_ClearError", CallingConvention = CallingConvention.Cdecl)]
-        public static extern void ClearError();
-
-
+        public static extern bool ClearError();
     }
 }
