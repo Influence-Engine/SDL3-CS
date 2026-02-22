935fa5055bc3d5c045dc4008c4a654f3a7c40027
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2026-02-22 14:37:27 +0100

    Update SDL_Rect.cs

--- SDL_Rect.cs
+++ SDL_Rect.cs
@@ -10,6 +10,15 @@ namespace SDL3
         {
             public int x; 
             public int y;
+
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public static readonly Point One = new Point(1, 1);
+            public static readonly Point Zero = new Point(0, 0);
         }
 
         /// <summary>Defines a point (using floats).</summary>
@@ -18,6 +27,15 @@ namespace SDL3
         {
             public float x;
             public float y;
+
+            public FPoint(float x, float y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public static readonly FPoint One = new FPoint(1f, 1f);
+            public static readonly FPoint Zero = new FPoint(0f, 0f);
         }
 
         /// <summary>A rectangle, with the origin at the upper left (using integers).</summary>
