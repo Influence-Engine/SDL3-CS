6c20b27c20a9cf51553d9581c817b7a1c229e1e1
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2026-02-23 14:31:28 +0100

    over engineered Points
    also broken o.o

--- SDL_Rect.cs
+++ SDL_Rect.cs
@@ -13,20 +13,25 @@ namespace SDL3
             public int x; 
             public int y;
 
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public Point(int x, int y)
             {
                 this.x = x;
                 this.y = y;
             }
 
-            public readonly int LengthSquared => x * x + y * y;
+            public readonly int LengthSquared
+            {
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                get => x * x + y * y;
+            }
 
             public readonly FPoint Normalized
             {
                 get
                 {
-                    float lengthSquared = LengthSquared;
-                    if (lengthSquared >= 0f)
+                    float lengthSquared = x * x + y * y;
+                    if (lengthSquared <= 0f)
                         return FPoint.Zero;
 
                     float invertLength = 1f / MathF.Sqrt(lengthSquared);
@@ -37,17 +42,52 @@ namespace SDL3
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public static FPoint Normalize(Point value) => value.Normalized;
 
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static int Dot(Point a, Point b) => a.x * b.x + a.y * b.y;
+
             public static readonly Point One = new Point(1, 1);
             public static readonly Point Zero = new Point(0, 0);
 
+            #region Operators
+
+            // Addition / Subtraction
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public static Point operator +(Point a, Point b) => new Point(a.x + b.x, a.y + b.y);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public static Point operator -(Point a, Point b) => new Point(a.x - b.x, a.y - b.y);
 
+            // Flip (Unary minus)
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static Point operator -(Point a) => new Point(-a.x, -a.y);
+
+            // Multiplication
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public static Point operator *(Point a, int scalar) => new Point(a.x * scalar, a.y * scalar);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static Point operator *(int scalar, Point a) => new Point(a.x * scalar, a.y * scalar);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public static Point operator *(Point a, Point b) => new Point(a.x * b.x, a.y * b.y);
+
+            // Division
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static Point operator /(Point a, int scalar) => new Point(a.x / scalar, a.y / scalar);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static Point operator /(Point a, Point b) => new Point(a.x / b.x, a.y / b.y);
+
+            // Same same but different
 
             public static bool operator ==(Point a, Point b) => a.x == b.x && a.y == b.y;
             public static bool operator !=(Point a, Point b) => !(a == b);
+
+            #endregion
 
             public override bool Equals(object? obj) => obj is Point p && this == p;
             public override int GetHashCode() => HashCode.Combine(x, y);
@@ -60,21 +100,26 @@ namespace SDL3
             public float x;
             public float y;
 
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public FPoint(float x, float y)
             {
                 this.x = x;
                 this.y = y;
             }
 
-            public readonly float LengthSquared => x * x + y * y;
+            public readonly float LengthSquared
+            {
+                [MethodImpl(MethodImplOptions.AggressiveInlining)]
+                get => x * x + y * y;
+            }
 
             public readonly FPoint Normalized
             {
                 get
                 {
-                    float lengthSquared = LengthSquared;
-                    if (lengthSquared >= 0f)
-                        return FPoint.Zero;
+                    float lengthSquared = x * x + y * y;
+                    if (lengthSquared <= 0f)
+                        return Zero;
 
                     float invertLength = 1f / MathF.Sqrt(lengthSquared);
                     return new FPoint(x * invertLength, y * invertLength);
@@ -84,17 +129,52 @@ namespace SDL3
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public static FPoint Normalize(FPoint value) => value.Normalized;
 
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static float Dot(FPoint a, FPoint b) => a.x * b.x + a.y * b.y;
+
             public static readonly FPoint One = new FPoint(1f, 1f);
             public static readonly FPoint Zero = new FPoint(0f, 0f);
 
+            #region Operators
+
+            // Addition / Subtraction
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public static FPoint operator +(FPoint a, FPoint b) => new FPoint(a.x + b.x, a.y + b.y);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public static FPoint operator -(FPoint a, FPoint b) => new FPoint(a.x - b.x, a.y - b.y);
 
+            // Flip (Unary minus)
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static FPoint operator -(FPoint a) => new FPoint(-a.x, -a.y);
+
+            // Multiplication
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public static FPoint operator *(FPoint a, float scalar) => new FPoint(a.x * scalar, a.y * scalar);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static FPoint operator *(float scalar, FPoint a) => new FPoint(a.x * scalar, a.y * scalar);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
             public static FPoint operator *(FPoint a, FPoint b) => new FPoint(a.x * b.x, a.y * b.y);
+
+            // Division
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static FPoint operator /(FPoint a, float scalar) => new FPoint(a.x / scalar, a.y / scalar);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public static FPoint operator /(FPoint a, FPoint b) => new FPoint(a.x / b.x, a.y / b.y);
+
+            // Same same but different
 
             public static bool operator ==(FPoint a, FPoint b) => a.x == b.x && a.y == b.y;
             public static bool operator !=(FPoint a, FPoint b) => !(a == b);
+
+            #endregion
 
             public override bool Equals(object? obj) => obj is FPoint p && this == p;
             public override int GetHashCode() => HashCode.Combine(x, y);
