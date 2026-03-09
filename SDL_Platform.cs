6f3c6047c640b786a50d1fc29afb10680e046d28
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2026-03-09 16:37:01 +0100

    Update SDL_Platform.cs

--- SDL_Platform.cs
+++ SDL_Platform.cs
@@ -1,15 +1,15 @@
-﻿using System;
+﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace SDL3
 {
     public static partial class SDL
     {
-        [DllImport(nativeLibraryName, EntryPoint = "SDL_GetPlatform", CallingConvention = CallingConvention.Cdecl)]
-        static extern IntPtr Internal_GetPlatform();
-
         /// <summary>Gets the name of the platform.</summary>
         /// <returns>The name of the platform. If the correct name is not available, returns string starting with "Unknown".</returns>
-        public static string GetPlatform() => Marshal.PtrToStringUTF8(Internal_GetPlatform());
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_GetPlatform")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        [return: MarshalAs(UnmanagedType.LPUTF8Str)]
+        public static partial string GetPlatform();
     }
 }
