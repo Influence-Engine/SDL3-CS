eafcddd6c4e2caf1687d8ba36869a137aec1678c
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2026-03-09 08:49:23 +0100

    Update SDL_Error.cs

--- SDL_Error.cs
+++ SDL_Error.cs
@@ -1,41 +1,39 @@
-﻿using System;
+﻿using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace SDL3
 {
     public static partial class SDL
     {
-       
-        [DllImport(nativeLibraryName, EntryPoint = "SDL_SetError", CallingConvention = CallingConvention.Cdecl)]
-        static extern unsafe bool Internal_SetError(byte* fmtAndArglist);
-
         /// <summary>Set the SDL error message for the current thread.</summary>
         /// <param name="fmtAndArglist">The error message</param>
         /// <returns>False</returns>
-        public static unsafe bool SetError(string fmtAndArglist)
-        {
-            int utf8FmtAndArlistBufferSize = Utility.UTF8Size(fmtAndArglist);
-            byte* utf8FmtAndArgList = stackalloc byte[utf8FmtAndArlistBufferSize];
-            return Internal_SetError(Utility.UTF8Encode(fmtAndArglist, utf8FmtAndArgList, utf8FmtAndArlistBufferSize));
-        }
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_SetError", StringMarshalling = StringMarshalling.Utf8)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        [return: MarshalAs(UnmanagedType.I1)]
+        public static partial bool SetError(string fmtAndArglist);
 
         // TODO SDL_SetErrorV
 
         /// <summary>Set an error indicating that memory allocation failed.</summary>
         /// <returns>False</returns>
-        [DllImport(nativeLibraryName, EntryPoint = "SDL_OutOfMemory", CallingConvention = CallingConvention.Cdecl)]
-        public static extern bool OutOfMemory();
-
-        [DllImport(nativeLibraryName, EntryPoint = "SDL_GetError", CallingConvention = CallingConvention.Cdecl)]
-        static extern IntPtr Internal_GetError();
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_OutOfMemory")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        [return: MarshalAs(UnmanagedType.I1)]
+        public static partial bool OutOfMemory();
 
         /// <summary>Retrieve a message about the last error that occurred on the current thread.</summary>
         /// <returns>The message with information about the specifiic error that occured, or NULL.</returns>
-        public static string GetError() => Marshal.PtrToStringUTF8(Internal_GetError());
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_GetError")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        [return: MarshalAs(UnmanagedType.LPUTF8Str)]
+        public static partial string? GetError();
 
         /// <summary>Clear any previous error message for this thread.</summary>
         /// <returns>True</returns>
-        [DllImport(nativeLibraryName, EntryPoint = "SDL_ClearError", CallingConvention = CallingConvention.Cdecl)]
-        public static extern bool ClearError();
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_ClearError")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        [return: MarshalAs(UnmanagedType.I1)]
+        public static partial bool ClearError();
     }
 }
