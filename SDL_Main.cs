20029fa177d406c5f0155ee1cb73b26a4d11baed
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2026-03-09 12:19:58 +0100

    Update SDL_Main.cs

--- SDL_Main.cs
+++ SDL_Main.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace SDL3
@@ -6,43 +7,120 @@ namespace SDL3
     public static partial class SDL
     {
 
-        [DllImport(nativeLibraryName, EntryPoint = "SDL_AppInit", CallingConvention = CallingConvention.Cdecl)]
-        public static extern AppResult AppInit(out IntPtr appState, int argc, IntPtr argv);
+        #region App callback delegates
 
-        [DllImport(nativeLibraryName, EntryPoint = "SDL_AppIterate", CallingConvention = CallingConvention.Cdecl)]
-        public static extern AppResult AppIterate(IntPtr appState);
+        /// <summary>
+        /// App-implemented intitial entry point for SDL_MAIN_USE_CALLBACKS apps. <br></br>
+        /// Called once at startup.
+        /// </summary>
+        /// <returns><see cref="AppResult.Continue"/> to proceed, <see cref="AppResult.Success"/> or <see cref="AppResult.Failure"/> to termiante.</returns>
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate AppResult AppInit_func(out nint appstate, int argc, nint argv);
 
-        [DllImport(nativeLibraryName, EntryPoint = "SDL_AppEvent", CallingConvention = CallingConvention.Cdecl)]
-        public static extern AppResult AppEvent(IntPtr appState, ref Event _event);
+        /// <summary>
+        /// App-implemted iteration entry point for SDL_MAIN_USE_CALLBACKS apps. <br></br>
+        /// Called repeatedly after <see cref="AppInit_func"/> returns <see cref="AppResult.Continue"/>
+        /// </summary>
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate AppResult AppIterate_func(nint appstate);
 
-        [DllImport(nativeLibraryName, EntryPoint = "SDL_AppQuit", CallingConvention = CallingConvention.Cdecl)]
-        public static extern void AppQuit(IntPtr appState, AppResult result);
+        /// <summary>
+        /// App-implemented event entry point for SDL_MAIN_USE_CALLBACKS apps. <br></br>
+        /// Called once before new event after <see cref="AppInit_func"/> returns <see cref="AppResult.Continue"/>
+        /// </summary>
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate AppResult AppEvent_func(nint appstate, ref Event sdlEvent);
 
-        public delegate int main(int argc, IntPtr argv);
+        /// <summary>
+        /// App-implemented deinit entry point for SDL_MAIN_USE_CALLBACKS apps. <br></br>
+        /// Called once before the program terminates, in all cases.
+        /// </summary>
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate void AppQuit_func(nint appstate, AppResult result);
 
-        /// <summary>An app-supplied function for program entry.</summary>
+        /// <summary>The prototype for the application's main() function.</summary>
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        public delegate int main_func(int argc, nint argv);
+
+        #endregion
+
+        /// <summary>
+        /// An app-supplied function for program entry.<br></br>
+        /// Apps should not call this directly. SDL redirects the platform entry point here.
+        /// </summary>
         /// <param name="argc">An ANSI-C style main function's argc.</param>
         /// <param name="argv">An ANSI-C style main function's argv</param>
         /// <returns>An ANSI-C main return code; generally 0 is considered successful program completion, and otherwise values are considered errors.</returns>
-        [DllImport(nativeLibraryName, EntryPoint = "SDL_main", CallingConvention = CallingConvention.Cdecl)]
-        public static extern int Main(int argc, IntPtr argv);
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_main")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        public static partial int Main(int argc, IntPtr argv);
 
-        /// <summary>Circumvent failure of Init() when not using main() as an entry point.</summary>
-        [DllImport(nativeLibraryName, EntryPoint = "SDL_SetMainReady", CallingConvention = CallingConvention.Cdecl)]
-        public static extern void SetMainReady();
+        /// <summary>
+        /// Circumvent failure of <see cref="Init"/> when not using <see cref="Main"/> as an entry point.
+        /// Must be called before <see cref="Init"/> if SDL_MAIN_HANDLED is defined.
+        /// </summary>
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_SetMainReady")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        public static partial void SetMainReady();
 
-        // TODO SDL_RunApp
+        /// <summary>
+        /// Intializes and launches an SDL application, performing platform-specific initialization 
+        /// before calling <paramref name="mainFunction"/> and cleanup after in returns. <br></br>
+        /// Use this when handling your own <see cref="Main"/> with SDL_MAIN_HANDLED. You do not need <see cref="SetMainReady"/> when using this.
+        /// </summary>
+        /// <param name="argc">The argc from the application's main(), or 0 if not available.</param>
+        /// <param name="argv">The argv from the application's main(), or NULL if not available.</param>
+        /// <param name="mainFunction">Your SDL app's C-style main function.</param>
+        /// <param name="reserved">Reserved for future use; pass NULL.</param>
+        /// <returns>The return value from <paramref name="mainFunction"/>: 0 on success, otherwise failure.</returns>
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_RunApp")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        public static partial int RunApp(int argc, nint argv, main_func mainFunction, nint reserved);
 
-        // TODO SDL_EnterAppMainCallbacks
+        /// <summary>
+        /// An entry point for SDL's use in SDL_MAIN_USE_CALLBACKS. <br></br>
+        /// Do not call this directly. It is invoked by SDL's magic header-only library.
+        /// </summary>
+        /// <param name="argc">Standard Unix main argc.</param>
+        /// <param name="argv">Standard Unix main argv.</param>
+        /// <param name="appInit">The application's <see cref="AppInit_func"/> function.</param>
+        /// <param name="appIter">The application's <see cref="AppIterate_func"/> function.</param>
+        /// <param name="appEvent">The application's <see cref="AppEvent_func"/> function.</param>
+        /// <param name="appQuit">The application's <see cref="AppQuit_func"/> function.</param>
+        /// <returns>Standard Unix main return value.</returns>
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_EnterAppMainCallbacks")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        public static partial int EnterAppMainCallbacks(int argc, nint argv, AppInit_func appInit, AppIterate_func appIter, AppEvent_func appEvent, AppQuit_func appQuit);
 
-        // TODO SDL_RegisterApp
+        /// <summary>
+        /// Register a Win32 window class for SDL's use (Windows only) <br></br>
+        /// Most applications do not need to call this directly. SDL calls it when initializing the video subsystem.
+        /// </summary>
+        /// <param name="name">The window class name in UTF-8, or null to use SDL's default.</param>
+        /// <param name="style">The value to use in WNDCLASSEX::style</param>
+        /// <param name="hInst">The HINSTANCE for WNDCLASSEX::hInstance, or zero to use GetModuleHandle(NULL).</param>
+        /// <returns>True on success or false on failure.</returns>
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_RegisterApp", StringMarshalling =StringMarshalling.Utf8)]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        [return: MarshalAs(UnmanagedType.I1)]
+        public static partial bool RegisterApp(string? name, uint style, nint hInst);
 
-        /// <summary>Deregister the win32 window class from an RegisterApp call.</summary>
-        [DllImport(nativeLibraryName, EntryPoint = "SDL_UnregisterApp", CallingConvention = CallingConvention.Cdecl)]
-        public static extern void UnregisterApp();
+        /// <summary>
+        /// Deregister the Win32 window class from a <see cref="RegisterApp"/> call. (Windows only) <br></br>
+        /// Most applications do not need to call this directly. SDL calls it when deinitializing the video subsystem.
+        /// </summary>
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_UnregisterApp")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        public static partial void UnregisterApp();
 
-        /// <summary>Callback from the application to let the suspend continue.</summary>
-        [DllImport(nativeLibraryName, EntryPoint = "SDL_GDKSuspendComplete", CallingConvention = CallingConvention.Cdecl)]
-        public static extern void GDKSuspendComplete();
+        /// <summary>
+        /// Callback from the application to let the GDK suspend continue. (Xbox GDK only) <br></br>
+        /// Should be called in response to SDL_EVENT_DID_ENTER_BACKGROUND from the render thread,
+        /// after suppressing all rendering operations. <br></br>
+        /// All other platforms do nothing.
+        /// </summary>
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_GDKSuspendComplete")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        public static partial void GDKSuspendComplete();
     }
 }
