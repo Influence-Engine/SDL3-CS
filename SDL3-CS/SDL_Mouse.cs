7869491e10e51314c3796a584a8ee1b0bad3ca2a
Quartzi <34374881+realQuartzi@users.noreply.github.com>
2026-04-10 13:16:44 +0200

    Mouse Complete

--- SDL3-CS/SDL_Mouse.cs
+++ SDL3-CS/SDL_Mouse.cs
@@ -166,6 +166,132 @@ namespace SDL3
 
         #endregion
 
+        #region Mouse Movement
+
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_WarpMouseInWindow")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl), typeof(CallConvSuppressGCTransition)])]
+        public static partial void WarpMouseInWindow(nint window, float x, float y);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void WarpMouseInWindow(nint window, FPoint position) => WarpMouseInWindow(window, position.x, position.y);
+
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_WarpMouseGlobal")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl), typeof(CallConvSuppressGCTransition)])]
+        [return: MarshalAs(UnmanagedType.I1)]
+        public static partial bool WarpMouseGlobal(float x, float y);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void WarpMouseGlobal(FPoint position) => WarpMouseGlobal(position.x, position.y);
+
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_SetRelativeMouseTransform")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        [return: MarshalAs(UnmanagedType.I1)]
+        public static partial bool SetRelativeMouseTransform(MouseMotionTransformCallback? callback, nint userdata);
+
+        #endregion
+
+        #region Relative Mouse Mode
+
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_SetWindowRelativeMouseMode")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl), typeof(CallConvSuppressGCTransition)])]
+        [return: MarshalAs(UnmanagedType.I1)]
+        public static partial bool SetWindowRelativeMouseMode(nint window, [MarshalAs(UnmanagedType.I1)] bool enabled);
+
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_GetWindowRelativeMouseMode")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl), typeof(CallConvSuppressGCTransition)])]
+        [return: MarshalAs(UnmanagedType.I1)]
+        public static partial bool GetWindowRelativeMouseMode(nint window);
+
+        #endregion
+
+        #region Mouse Capture
+
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_CaptureMouse")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl), typeof(CallConvSuppressGCTransition)])]
+        [return: MarshalAs(UnmanagedType.I1)]
+        public static partial bool CaptureMouse([MarshalAs(UnmanagedType.I1)] bool enabled);
+
+        #endregion
+
+        #region Cursor Creation
+
+        /// <summary>Create a cursor using bitmap data and mask (in MSB format).</summary>
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_CreateCursor")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        public static unsafe partial nint CreateCursor(byte* data, byte* mask, int w, int h, int hotX, int hotY);
+
+        /// <summary>Create a color cursor from a surface.</summary>
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_CreateColorCursor")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        public static partial nint CreateColorCursor(nint surface, int hotX, int hotY);
+
+        /// <summary>Create an animated color cursor.</summary>
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_CreateAnimatedCursor")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl)])]
+        public static unsafe partial nint CreateAnimatedCursor(CursorFrameInfo* frames, int frameCount, int hotX, int hotY);
+
+        /// <summary>Create an animated color cursor from a span.</summary>
+        public static unsafe nint CreateAnimatedCursor(ReadOnlySpan<CursorFrameInfo> frames, int hotX, int hotY)
+        {
+            fixed (CursorFrameInfo* ptr = frames)
+            {
+                return CreateAnimatedCursor(ptr, frames.Length, hotX, hotY);
+            }
+        }
+
+        /// <summary>Create a system cursor.</summary>
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_CreateSystemCursor")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl), typeof(CallConvSuppressGCTransition)])]
+        public static partial nint CreateSystemCursor(SystemCursor id);
+
+        #endregion
+
+        #region Cursor Management
+
+        /// <summary>Set the active cursor.</summary>
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_SetCursor")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl), typeof(CallConvSuppressGCTransition)])]
+        [return: MarshalAs(UnmanagedType.I1)]
+        public static partial bool SetCursor(nint cursor);
+
+        /// <summary>Get the active cursor.</summary>
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_GetCursor")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl), typeof(CallConvSuppressGCTransition)])]
+        public static partial nint GetCursor();
+
+        /// <summary>Set the default cursor.</summary>
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_GetDefaultCursor")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl), typeof(CallConvSuppressGCTransition)])]
+        public static partial nint GetDefaultCursor();
+
+        /// <summary>Free a previously-created cursor.</summary>
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_DestroyCursor")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl), typeof(CallConvSuppressGCTransition)])]
+        public static partial void DestroyCursor(nint cursor);
+
+        #endregion
+
+        #region Cursor Visibilty
+
+        /// <summary>Show the cursor.</summary>
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_ShowCursor")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl), typeof(CallConvSuppressGCTransition)])]
+        [return: MarshalAs(UnmanagedType.I1)]
+        public static partial bool ShowCursor();
+
+        /// <summary>Hide the cursor.</summary>
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_HideCursor")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl), typeof(CallConvSuppressGCTransition)])]
+        [return: MarshalAs(UnmanagedType.I1)]
+        public static partial bool HideCursor();
+
+        /// <summary>Return whether the cursor is currently being shown.</summary>
+        [LibraryImport(nativeLibraryName, EntryPoint = "SDL_CursorVisible")]
+        [UnmanagedCallConv(CallConvs = [typeof(CallConvCdecl), typeof(CallConvSuppressGCTransition)])]
+        [return: MarshalAs(UnmanagedType.I1)]
+        public static partial bool CursorVisible();
+
+        #endregion
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsMouseButtonPressed(uint state, MouseButton button) => (state & (1u << ((int)button - 1))) != 0;
